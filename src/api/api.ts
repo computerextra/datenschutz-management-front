d0ac37613a9ed1715cf587e271600087b156c56b
Johannes Kirchner (Computer Extra) <service@computer-extra.de>
2023-11-27 12:31:54 +0100

    added api URl in aou

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.API_URL || "http://localhost:3001",
 });
 
 export default client;
